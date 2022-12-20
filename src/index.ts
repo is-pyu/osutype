094e15ab462c21aca105a872b1a13d0c33f6800b
is-pyu <tom7hong7@gmail.com>
2022-12-20 14:02:26 +0900

    make it actually work

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,10 @@ const nonSpamKeycodes = [
   16,  17,   18,   91,   93,   20,   27,   112,  113,  114,  115,  116,  117,  118,  119,  120,  121,  122,  123,   45,   46,  19,   144,   37,  38,  39,   40
 ];
 const validKeycodes = [
-  8,  9,  13,  16,  17,  18,  19,  20,  27,  32,  33,  34, 35,  36,  37,  38,  39,  40,  45,  46,  48,  49,  50,  51,  52,  53,  54,  55,  56,  57,  65,  66,  67,  68,  69,  70,  71,  72,  73,  74,  75,  76,  77,  78,  79,  80,  81,  82,  83,  84,  85,  86,  87,  88,  89,  90,  91,  92,  93,  96,  97,  98,  99,  100,  101,  102,  103,  104,  105,  106,  107,  109,  110,  111,  112,  113,  114,  115,  116,  117,  118,  119,  120,  121,  122,  123,  144,  145,  186,  187,  188,  189,  190,  191,  192,  193,  194,  219,  220,  221,  222,  223,  224,  225,  226,  227,  228,  229,  230,  231,  232,  233,  234,  235,  236,  237,
+  8,  9,  13,  16,  17,  18,  19,  20,  27,  32,  33,  34, 35,  36,  37,  38,  39,  40,  45,  46,  48,  49,  50,  51,  52,  53,  54,  55,  56,  57,  65,  66,  67,  68,  69,  70,  71,  72,  73,  74,  75,  76,  77,  78,  79,  80,  81,  82,  83,  84,  85,  86,  87,  88,  89,  90,  91,  92,  93,  96,  97,  98,  99,  100,  101,  102,  103,  104,  105,  106,  107,  109,  110,  111,  112,  113,  114,  115,  116,  117,  118,  119,  120,  121,  122,  123,  144,  145,  186,  187,  188,  189,  190,  191,  192,  193,  194,  219,  220,  221,  222,  223,  224,  225,  226,  227,  228,  229,  230,  231,  232,  233,  234,  235,  236,  237
 ];
 
-export function startPlugin(): void {
+export function start(): void {
     //add an audio element to the page root
     const audio = document.createElement("audio");
     audio.src = "https://github.com/happyori/OsuTyping/raw/master/menuclick.wav";
@@ -33,7 +33,7 @@ export function startPlugin(): void {
         }
     )*/
 }
-export function pluginWillUnload(): void {
+export function stop(): void {
     //remove the audio element from the page root
     document.body.removeChild(document.getElementById("osutype") as HTMLInputElement);
     document.removeEventListener("keydown", (e: KeyboardEvent) => {
@@ -47,14 +47,14 @@ export function pluginWillUnload(): void {
 }
 export function keyDown(e: KeyboardEvent): void {
     //seek the audio element to 0 seconds and play it
-    if (validKeycodes.includes(e.keyCode)) {
+    if (e.isComposing || validKeycodes.includes(e.keyCode)) {
         const keyCode: number = e.keyCode;
         const value = document.activeElement!.attributes.getNamedItem('role')?.value;
         const value2 = document.activeElement!.tagName.toLowerCase();
         if (value == 'textbox' || value == 'combobox' || value2 == 'input') {
             if (nonSpamKeycodes.includes(keyCode) && keyCode == lastKey) {return;}
-            //(document.getElementById("osutype") as HTMLInputElement).currentTime = 0;
-            //(document.getElementById("osutype") as HTMLInputElement).play();
+            (document.getElementById("osutype") as HTMLInputElement).currentTime = 0;
+            (document.getElementById("osutype") as HTMLInputElement).play();
             lastKey = keyCode;
         }
     }
